5408db5da344640d8b9e46b99c2cc9d4d5e70940
MayumyCH <heydy.carrasco.huaccha@gmail.com>
2022-06-09 02:46:39 -0500

    feat: add files initial

---
+++ readme.md
@@ -0,0 +1,21 @@
+
+# streamlit_data_project_python
+
+
+PASOS:
+
+1. Crear un Enviroments Python
+
+python3 -m venv envStreamlit
+python --version
+source envStreamlit/Scripts/activate
+
+Link para crear enviroment de diversos modos:
+https://gist.github.com/MayumyCH/8641ce303572488239692db3a07f2334
+
+Version de python: Python 3.8.8
+
+Instalar las librerias necesarias:
+pip install -r ./resources/requirements.txt
+
+2. 
---
+++ web.py
@@ -0,0 +1,44 @@
+# Importar librerias
+import streamlit as st
+import pickle
+import pandas as pd
+
+# Extrar los archivos pickle
+with open('knn_model.pkl', 'rb') as li: # Modo Lectura
+    lin_reg = pickle.load(li)
+
+with open('lr_model.pkl', 'rb') as lo:
+    log_reg = pickle.load(lo)
+
+with open('rf_model.pkl', 'rb') as sv:
+    svc_m = pickle.load(sv)
+
+
+def main():
+
+
+    # TITULO
+    st.title('Predicción de ventas de videojuegos')
+    st.header("Introducción ")
+    st.write("""
+     proyecto se propone el análisis de la data proporcionada por Kaggle el cual corresponde a videojuegos se tiene las ventas y 
+     los scores obtenidos (Vgchartz y Metacritic); se busca Predecir la ventas de videojuegos para el género de Acción.
+    """)
+
+    # -- SIDEBAR
+    st.sidebar.header('Ingresar Parametros')
+
+
+    #escoger el modelo preferido
+    option = ['Linear Regression', 'KNN', 'SVM']
+    model = st.sidebar.selectbox('Que modelo vas a usar?', option)
+
+
+    # Segundo encabezado
+    st.subheader('Modelo: ')
+    st.subheader(model)
+    #st.write(df)
+
+
+if __name__ == '__main__':
+    main()
